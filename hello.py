ad039733b17f1d446ee10c57207a6abde05a1ee1
Letícia Brondi Carvalheiro <l.brondi@aluno.ifsp.edu.br>
2026-08-10 21:40:50 -0300

    update: avaliação contínua 04, templates

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+import flask  # Para pegar a versão do Flask
 
 app = Flask(__name__)
 
@@ -10,10 +11,18 @@ moment = Moment(app)
 
 # Configurações do aluno
 ALUNO = {
-    'nome': 'Letícia Brondi',
-    'prontuario': 'PT3037801',
+    'nome': 'Fabio Teixeira',
+    'prontuario': 'PT23820X',
     'instituicao': 'IFSP'
 }
+
+# Context processor para injetar variáveis globais em todos os templates
+@app.context_processor
+def inject_globals():
+    return {
+        'flask_version': flask.__version__,
+        'aluno': ALUNO
+    }
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -34,13 +43,12 @@ def index():
 def identificacao():
     """Rota Identificação - dados do aluno"""
     return render_template('identificacao.html',
-                         aluno=ALUNO,
                          titulo='Identificação')
 
 @app.route('/contextorequisicao')
 def contextorequisicao():
     """Rota Contexto da Requisição - dados do navegador e servidor"""
-    user_agent = request.headers.get('User-Agent')
+    user_agent = request.headers.get('User-Agent', 'Desconhecido')
     remote_ip = request.remote_addr
     host = request.host
     
@@ -48,12 +56,14 @@ def contextorequisicao():
                          user_agent=user_agent,
                          remote_ip=remote_ip,
                          host=host,
-                         aluno=ALUNO,
                          titulo='Contexto da requisição')
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    """Rota dinâmica de usuário"""
+    return render_template('user.html', 
+                         name=name,
+                         titulo=f'Usuário {name}')
 
 if __name__ == '__main__':
     app.run(debug=True)
